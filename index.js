f1b07c004764206223a07aaf69c762f06ceb0688
MJU <marie.ju2012@outlook.com>
2020-03-10 22:29:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+// returns an object with the orignal key value pairs and the new key value pair
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {key: value})
+}
+
+// updates `object` with the given `key` and `value` (it is destructive) and
+// returns the entire updated object
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+// deletes `key` from a clone of object and
+// returns the new object (it is non-destructive)
+function deleteFromObjectByKey(object, key) {
+  return delete object[key]
+}
+
+// returns object without the delete key/value pair
+// modifiy original object
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
